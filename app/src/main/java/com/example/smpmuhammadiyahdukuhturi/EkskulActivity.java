524fc0c6086884d2ba02fd58f02fcc5a37619bc4
Sakuraifikar <fikaryourwelcome@gmail.com>
2022-03-10 09:11:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/smpmuhammadiyahdukuhturi/EkskulActivity.java
@@ -0,0 +1,14 @@
+package com.example.smpmuhammadiyahdukuhturi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EkskulActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ekskul);
+    }
+}
---
+++ app/src/main/java/com/example/smpmuhammadiyahdukuhturi/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.smpmuhammadiyahdukuhturi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
+public class MainActivity extends AppCompatActivity {
+    SliderView sliderView;
+    int[] images = {R.drawable.sklh1,
+            R.drawable.sklh2,
+            R.drawable.sklh3,
+            R.drawable.sklh1,
+            R.drawable.sklh2};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sliderView = findViewById(R.id.image_slider);
+
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
+    }
+
+    public void profile(View view) {
+        Intent intent = new Intent(MainActivity.this, ProfilActivity.class);
+        startActivity(intent);
+    }
+
+    public void galeri(View view) {
+        Intent intent = new Intent(MainActivity.this, GaleriActivity.class);
+        startActivity(intent);
+    }
+
+    public void ekskul(View view) {
+        Intent intent = new Intent(MainActivity.this, EkskulActivity.class);
+        startActivity(intent);
+    }
+
+    public void fasilitas(View view) {
+        Intent intent = new Intent(MainActivity.this, FasilitasActivity.class);
+        startActivity(intent);
+    }
+
+    public void guru(View view) {
+        Intent intent = new Intent(MainActivity.this, GuruActivity.class);
+        startActivity(intent);
+    }
+
+    public void prestasi(View view) {
+        Intent intent = new Intent(MainActivity.this, PrestasiActivity.class);
+        startActivity(intent);
+    }
+}
