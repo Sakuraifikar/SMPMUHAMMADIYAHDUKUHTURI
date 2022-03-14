00a8d7244d3592534ae26fc8909b2bc0e27c1a89
Sakuraifikar <fikaryourwelcome@gmail.com>
2022-03-14 11:04:05 +0700

    tomoki

--- app/src/main/java/com/example/smpmuhammadiyahdukuhturi/EkskulActivity.java
+++ app/src/main/java/com/example/smpmuhammadiyahdukuhturi/EkskulActivity.java
@@ -2,13 +2,53 @@ package com.example.smpmuhammadiyahdukuhturi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
 
 public class EkskulActivity extends AppCompatActivity {
+    SliderView sliderView;
+    int[] images = {R.drawable.sklh1,
+            R.drawable.sklh2,
+            R.drawable.sklh3,
+            R.drawable.sklh1,
+            R.drawable.sklh2};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ekskul);
+        sliderView = findViewById(R.id.image_slider);
+
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
+    }
+
+    public void osis(View view) {
+        Intent intent = new Intent(EkskulActivity.this, OsisActivity.class);
+        startActivity(intent);
+    }
+
+    public void pramuka(View view) {
+        Intent intent = new Intent(EkskulActivity.this, PramukaActivity.class);
+        startActivity(intent);
+    }
+
+    public void pmr(View view) {
+        Intent intent = new Intent(EkskulActivity.this, PmrActivity.class);
+        startActivity(intent);
+    }
+
+    public void futsal(View view) {
+        Intent intent = new Intent(EkskulActivity.this, FutsalActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/smpmuhammadiyahdukuhturi/GaleriActivity.java
+++ app/src/main/java/com/example/smpmuhammadiyahdukuhturi/GaleriActivity.java
@@ -2,7 +2,9 @@ package com.example.smpmuhammadiyahdukuhturi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class GaleriActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class GaleriActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_galeri);
+    }
+
+    public void satu(View view) {
+        Intent intent = new Intent(GaleriActivity.this, AlbumdrumbandActivity.class);
+        startActivity(intent);
+    }
+
+    public void dua(View view) {
+        Intent intent = new Intent(GaleriActivity.this, AlbumfutsalActivity.class);
+        startActivity(intent);
+    }
+
+    public void tiga(View view) {
+        Intent intent = new Intent(GaleriActivity.this, AlbumkegiatanActivity.class);
+        startActivity(intent);
     }
 }
