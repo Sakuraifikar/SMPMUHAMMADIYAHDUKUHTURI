57b25b48cdeefb217a0e0d8acb50896ee5c4868f
Sakuraifikar <fikaryourwelcome@gmail.com>
2022-03-16 08:36:57 +0700

    nymph

--- app/src/main/java/com/example/smpmuhammadiyahdukuhturi/Buku7Activity.java
+++ app/src/main/java/com/example/smpmuhammadiyahdukuhturi/Buku7Activity.java
@@ -2,7 +2,10 @@ package com.example.smpmuhammadiyahdukuhturi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class Buku7Activity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class Buku7Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buku7);
+    }
+
+
+    public void b7(View view) {
+        goToUrl("https://drive.google.com/file/d/17zaH5O0YRtMNueKxCKeNv2U1_An7QOiK/view?usp=drivesdk");
+    }
+
+    public void b8(View view) {
+        goToUrl("https://drive.google.com/file/d/17i5FpSXODOm_j3SBltygrub5mRmlCLS1/view?usp=drivesdk");
+    }
+
+    public void b9(View view) {
+        goToUrl("https://drive.google.com/file/d/17ohLAJooXRxPksCppQPFyVcmzS0v64xS/view?usp=drivesdk");
+    }
+
+    private void goToUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
--- app/src/main/java/com/example/smpmuhammadiyahdukuhturi/Buku8Activity.java
+++ app/src/main/java/com/example/smpmuhammadiyahdukuhturi/Buku8Activity.java
@@ -2,7 +2,10 @@ package com.example.smpmuhammadiyahdukuhturi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class Buku8Activity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class Buku8Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buku8);
+    }
+
+    public void a1(View view) {
+        goToUrl("https://drive.google.com/file/d/17zaH5O0YRtMNueKxCKeNv2U1_An7QOiK/view?usp=drivesdk");
+    }
+
+    public void a2(View view) {
+        goToUrl("https://drive.google.com/file/d/17i5FpSXODOm_j3SBltygrub5mRmlCLS1/view?usp=drivesdk");
+    }
+
+    public void a3(View view) {
+        goToUrl("https://drive.google.com/file/d/17ohLAJooXRxPksCppQPFyVcmzS0v64xS/view?usp=drivesdk");
+    }
+
+    private void goToUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
