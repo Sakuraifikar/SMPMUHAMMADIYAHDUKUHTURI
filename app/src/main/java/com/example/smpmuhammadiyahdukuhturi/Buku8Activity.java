f94e71d8353995dc28476108df3eacb1a4e81899
Sakuraifikar <fikaryourwelcome@gmail.com>
2022-03-30 10:50:30 +0700

    buku

--- app/src/main/java/com/example/smpmuhammadiyahdukuhturi/Buku8Activity.java
+++ app/src/main/java/com/example/smpmuhammadiyahdukuhturi/Buku8Activity.java
@@ -15,17 +15,58 @@ public class Buku8Activity extends AppCompatActivity {
         setContentView(R.layout.activity_buku8);
     }
 
-    public void a1(View view) {
-        goToUrl("https://drive.google.com/file/d/17zaH5O0YRtMNueKxCKeNv2U1_An7QOiK/view?usp=drivesdk");
+    public void mat7(View view) {
+        goToUrl("https://drive.google.com/file/d/1QXpoCmuE9qx2lxvllD5u2nntsGpC6zUQ/view?usp=sharing");
     }
 
-    public void a2(View view) {
-        goToUrl("https://drive.google.com/file/d/17i5FpSXODOm_j3SBltygrub5mRmlCLS1/view?usp=drivesdk");
+    public void mat72(View view) {
+        goToUrl("https://drive.google.com/file/d/1R5h8H9nmJU72m9WITVZmBLdqwdnie0eV/view?usp=sharing");
     }
 
-    public void a3(View view) {
-        goToUrl("https://drive.google.com/file/d/17ohLAJooXRxPksCppQPFyVcmzS0v64xS/view?usp=drivesdk");
+    public void ipa1kls7(View view) {
+        goToUrl("https://drive.google.com/file/d/1IVlMAu5WvVR5WCMfXqzqXmyJfEN1KViQ/view?usp=sharing");
     }
+
+    public void ipa2kls7(View view) {
+        goToUrl("https://drive.google.com/file/d/1QNjkCap3sIVcqhwax4sjU6tJJdrNNi4I/view?usp=sharing");
+    }
+
+    public void pra1kls7(View view) {
+        goToUrl("https://drive.google.com/file/d/1QDSKJmU-TBtyPsthsk4xDOcyePCDqlMi/view?usp=sharing");
+    }
+
+    public void pra2kls7(View view) {
+        goToUrl("https://drive.google.com/file/d/1Qi51Er20t455i5hlLUDs91e6r8L1OW3L/view?usp=sharing");
+    }
+
+    public void indkls7(View view) {
+        goToUrl("https://drive.google.com/file/d/1QJi9YctnB1aac2WLgm7j-7jEA-vzfwDQ/view?usp=sharing");
+    }
+
+    public void ingkls7(View view) {
+        goToUrl("https://drive.google.com/file/d/1QMN-DaOtw7rA5IzRLBCLsljKgvxc3hMs/view?usp=sharing");
+    }
+
+    public void ppkn7(View view) {
+        goToUrl("https://drive.google.com/file/d/1IHQihHJPuT2Wjv6whNvwtgcXDXX_pjMO/view?usp=sharing");
+    }
+
+    public void paikls7(View view) {
+        goToUrl("https://drive.google.com/file/d/1QbEAk6y-Veee89ga4J9AQyC1xJ-4fmfo/view?usp=sharing");
+    }
+
+    public void penjas7(View view) {
+        goToUrl("https://drive.google.com/file/d/1IH2zX5IZNIUUjL-xHT3eOS2HE8QJDipw/view?usp=sharing");
+    }
+
+    public void sbykls7(View view) {
+        goToUrl("https://drive.google.com/file/d/1QkM2zZghoT0BJUKLZF2Bc42hwqwiy3G0/view?usp=sharing");
+    }
+
+    public void ipskls7(View view) {
+        goToUrl("https://drive.google.com/file/d/1QTT70ghMSve9x1N8VKC14BUzWfJJhcrR/view?usp=sharing");
+    }
+
 
     private void goToUrl(String url) {
         Uri uriUrl = Uri.parse(url);
--- app/src/main/java/com/example/smpmuhammadiyahdukuhturi/PrestasiActivity.java
+++ app/src/main/java/com/example/smpmuhammadiyahdukuhturi/PrestasiActivity.java
@@ -6,33 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
-import com.smarteist.autoimageslider.SliderAnimations;
-import com.smarteist.autoimageslider.SliderView;
-
 public class PrestasiActivity extends AppCompatActivity {
-    SliderView sliderView;
-    int[] images = {R.drawable.sklh1,
-            R.drawable.sklh2,
-            R.drawable.sklh3,
-            R.drawable.sklh1,
-            R.drawable.sklh2};
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prestasi);
-        sliderView = findViewById(R.id.image_slider);
-
-        SliderAdapter sliderAdapter = new SliderAdapter(images);
-
-        sliderView.setSliderAdapter(sliderAdapter);
-        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
-        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
-        sliderView.startAutoCycle();
     }
-
 
     public void buku7(View view) {
         Intent intent = new Intent(PrestasiActivity.this, Buku7Activity.class);
